d3c8ac4816ba64ce1c003320472ffd613f0f7578
s2847547 <s2847547@students.fscj.edu>
2026-08-13 15:21:47 -0400

    feat: add song class from blank-file drill

---
+++ src/ch1_2/Song.java
@@ -0,0 +1,29 @@
+package ch1_2;
+
+public class Song {
+    String title;
+    String lengthSeconds;
+    static int songCount = 0;
+
+    Song(String title, String lengthSeconds) {
+        this.title = title;
+        this.lengthSeconds = lengthSeconds;
+        songCount = songCount + 1;
+    }
+
+    String summary() {
+        return "Title: " + title + "\n" +
+                "Length Seconds: " + lengthSeconds + "\n" +
+                "...\n" +
+                "Songs added: " + songCount;
+    }
+
+    public static void main(String[] args) {
+
+        Song song1 = new Song("Let Me Love You", "214");
+        Song song2 = new Song("Sandstorm", "315");
+
+        System.out.println(song1.summary());
+        System.out.println(song2.summary());
+    }
+}
